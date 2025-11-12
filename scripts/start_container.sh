3c8dd4a83721a1b22eca45114f09bd4aefbc5ac4
jacksamson1503 <jacksamsonc@gmail.com>
2025-11-12 21:42:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-#pull the Docker image from docker hub
-docker push jack1503/simple-python-flaskapp:tagname
+# Pull the Docker image from Docker Hub
+docker pull jack1503/simple-python-flaskapp:latest
 
-#run the docker image as a container
-docker run -d - 5000:5000 jack1503/simple-python-flaskapp:tagname
+# Stop any existing container
+docker stop simple-python-flaskapp || true
+docker rm simple-python-flaskapp || true
+
+# Run the new container
+docker run -d -p 5000:5000 --name simple-python-flaskapp jack1503/simple-python-flaskapp:latest
